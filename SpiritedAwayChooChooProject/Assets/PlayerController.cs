50ce35716a34b55e46594a3eab64efc336cdb3ee
RNewlinUCSC <102611892+RNewlinUCSC@users.noreply.github.com>
2022-10-29 23:35:05 -0700

    This is the Start
    Train greybox added. Player added, and controls mapped.

---
+++ SpiritedAwayChooChooProject/Assets/PlayerController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("References")]
+    public Rigidbody rb;
+    public Transform head;
+    public Camera Camera;
+
+    [Header("Configurations")]
+    public float walkSpeed;
+    public float runSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Horizontal Rotation
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * 2f);
+    }
+
+    void FixedUpdate() 
+    {
+        Vector3 newVelocity = Vector3.up * rb.velocity.y;
+        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+        newVelocity.x = Input.GetAxis("Horizontal") * speed;
+        newVelocity.z = Input.GetAxis("Vertical") * speed;
+        rb.velocity = transform.TransformDirection(newVelocity);
+    }
+
+    void LateUpdate() {
+        //Vertical Rotation
+        Vector3 e = head.eulerAngles;
+        e.x -= Input.GetAxis("Mouse Y") * 2f;
+        e.x = RestrictAngle(e.x, -85f, 85f);
+        head.eulerAngles = e;
+    }
+
+    // Clamp the vertical head rotation (prevent breaking the players neck)
+    public static float RestrictAngle(float angle, float angleMin, float angleMax) {
+        if(angle > 180) {
+            angle -= 360;
+        }
+        else if(angle < -180) {
+            angle += 360;
+        }
+
+        if(angle > angleMax) {
+            angle = angleMax;
+        }
+        if(angle < angleMin) {
+            angle = angleMin;
+        }
+
+        return angle;
+    }
+}
