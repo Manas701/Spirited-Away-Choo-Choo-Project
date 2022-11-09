5570bdb239acfe1723ae47e71f86e2cbb8a5f809
Huy <mitanguy@ucsc.edu>
2022-11-08 16:16:18 -0800

    Revert "Revert "Revert "Revert "Changed text style and added dialogue box.""""
    This reverts commit 47060acb19dacc922fea0e0c124fdb03ba7663b8.

--- SpiritedAwayChooChooProject/Assets/Scripts/InteractableObject.cs
+++ SpiritedAwayChooChooProject/Assets/Scripts/InteractableObject.cs
@@ -20,6 +20,7 @@ public class InteractableObject : MonoBehaviour
     public bool justEnded = false;
     public bool takenInteraction = false;
     private bool cantInteract = false;
+    public Animator animator;
 
     public List<AudioClip> talkingClips;
 
@@ -73,6 +74,7 @@ public class InteractableObject : MonoBehaviour
                 }
                 if (d.sentences.Length == 0)
                 {
+                    animator.SetBool("isOpen", true);
                     p.canMove = false;
                     d.sentences = iSentences;
                     d.hasTextField = hasTextField;
@@ -86,6 +88,7 @@ public class InteractableObject : MonoBehaviour
                     d.NextSentence();
                     if (d.index == d.sentences.Length && canTrueMove == true)
                     {
+                        animator.SetBool("isOpen", false);
                         p.canMove = true;
                         justEnded = true;
                     }
